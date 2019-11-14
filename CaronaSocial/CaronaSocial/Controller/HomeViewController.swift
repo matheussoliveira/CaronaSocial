71359dc65df1dbe37b913b3e36ff35b42864f68c
LunaFelipe <felipe.luna.tersi@gmail.com>
2019-11-14 12:13:17 -0300

    screen home

---
+++ CaronaSocial/CaronaSocial/Controller/HomeViewController.swift
@@ -0,0 +1,46 @@
+//
+//  HomeViewController.swift
+//  CaronaSocial
+//
+//  Created by Felipe Luna Tersi on 14/11/19.
+//  Copyright © 2019 Matheus Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = homeTableView.dequeueReusableCell(withIdentifier: "cell") as! HomeTableViewCell
+
+        
+        return cell
+        
+    }
+
+    @IBOutlet weak var homeTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
