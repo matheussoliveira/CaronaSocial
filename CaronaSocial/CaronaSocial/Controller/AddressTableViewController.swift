6e536c5d1b3ecc6f4ec2ee4b10954c5e24301b24
LunaFelipe <felipe.luna.tersi@gmail.com>
2019-12-02 10:40:37 -0300

    edit select adress done

--- CaronaSocial/CaronaSocial/Controller/AddressTableViewController.swift
+++ CaronaSocial/CaronaSocial/Controller/AddressTableViewController.swift
@@ -13,6 +13,7 @@ class AddressTableViewController: UITableViewController {
     @IBOutlet var adressTableView: UITableView!
     
     var name:[String] = []
+    var fieldFlag: String?
     
     @IBAction func confirmButton(_ sender: Any) {
         
@@ -20,14 +21,7 @@ class AddressTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         name = ["Casa", "Trabalho", "Institutição"]
-
-    // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -43,6 +37,29 @@ class AddressTableViewController: UITableViewController {
         
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "backToEditRote" && fieldFlag == "start" {
+            if let adressVC = segue.destination as? EditRoteTableViewController {
+              adressVC.startText = sender as? String
+            }
+        }
+        
+        if segue.identifier == "backToEditRote" && fieldFlag == "destiny" {
+            if let adress2VC = segue.destination as? EditRoteTableViewController {
+              adress2VC.destinyText = sender as? String
+            }
+        }
+        
+        if segue.identifier == "newAdressSegue" {
+            if let newAdressVC = segue.destination as? NewAddressTableViewController {
+                newAdressVC.adressList = self
+            }
+        }
+    }
+    
+    // MARK: - Table view data source
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return name.count
     }
@@ -52,25 +69,13 @@ class AddressTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "AdressTableViewCell") as! AddressTableViewCell
         
         cell.adress.text = name[indexPath.row]
+        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         self.performSegue(withIdentifier: "backToEditRote", sender: name[indexPath.row])
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "backToEditRote" {
-            if let adressVC = segue.destination as? EditRoteTableViewController {
-              adressVC.adress = sender as? String
-            }
-        }
         
-        if segue.identifier == "newAdressSegue" {
-            if let newAdressVC = segue.destination as? NewAddressTableViewController {
-                newAdressVC.adressList = self
-            }
-        }
     }
 }
--- CaronaSocial/CaronaSocial/Controller/EditRoteTableViewController.swift
+++ CaronaSocial/CaronaSocial/Controller/EditRoteTableViewController.swift
@@ -15,103 +15,88 @@ class EditRoteTableViewController: UITableViewController, UIPickerViewDelegate, 
     @IBOutlet weak var destiny: UITextField!
     @IBOutlet weak var arrivalTime: UITextField!
     
-    var adress: String?
+    var startText: String?
+    var destinyText: String?
     var rote: MatchsTableViewController?
-    
     var selectedHour: String = ""
     var selectedMinute: String = ""
+    var selectedField: String?
     
-    var hours =
-        ["01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","00"]
+    var hours = ["01", "02", "03", "04", "05", "06", "07", "08",
+                 "09", "10", "11", "12", "13", "14", "15", "16",
+                 "17", "18", "19", "20", "21", "22", "23", "00"]
     
-    var minutes = ["00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59"]
+    var minutes = ["00", "01", "02", "03", "04", "05", "06", "07",
+                   "08", "09", "10", "11", "12", "13", "14", "15",
+                   "16", "17", "18", "19", "20", "21", "22", "23",
+                   "24", "25", "26", "27", "28", "29", "30", "31",
+                   "32", "33", "34", "35", "36", "37", "38", "39",
+                   "40", "41", "42", "42", "43", "44", "45", "46",
+                   "47", "48", "49", "50", "51", "52", "53", "54",
+                   "55", "56", "57", "58", "59"]
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        
         let datePicker = UIPickerView()
-        
         datePicker.delegate = self
         datePicker.dataSource = self
-        
         departureTime.inputView = datePicker
-
         arrivalTime.inputView = datePicker
-        
         showDatePicker()
-        
-//        let datePicker = UIDatePicker()
-//        datePicker.datePickerMode = UIDatePicker.Mode.time
-//
-//        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
-//
-//        view.addGestureRecognizer(tapGesture)
-//
-//        departureTime.inputView = datePicker
     }
-    
-    func showDatePicker(){
-        
-        //ToolBar
-        let toolbar = UIToolbar();
-        toolbar.sizeToFit()
-//        let doneButton = UIBarButtonItem(title: "Cancelar", style: .plain, target: self, action: #selector(cancelConditionPicker));
-        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
-        let cancelButton = UIBarButtonItem(title: "Confirmar", style: .plain, target: self, action: #selector(doneConditionPicker));
-        
-        toolbar.tintColor = #colorLiteral(red: 0.7607844472, green: 0.235294193, blue: 0.5333334208, alpha: 1)
-        toolbar.setItems([spaceButton,cancelButton], animated: false)
-        
-        departureTime.inputAccessoryView = toolbar
-        arrivalTime.inputAccessoryView = toolbar
-    }
-    
-    @objc func doneConditionPicker(){
-        self.view.endEditing(true)
-    }
-    
-//    @objc func cancelConditionPicker(){
-//        self.view.endEditing(true)
-//    }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         
-        start.text = adress
-
+        start.text = startText
+        destiny.text = destinyText
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        // Sending information to MatchesTableViewController
+        if segue.identifier == "1" {
+            if let adressVC = segue.destination as? EditRoteTableViewController {
+                adressVC.startText = sender as? String
+            }
+        }
+        
+        if segue.identifier == "2" {
+            if let adressVC = segue.destination as? EditRoteTableViewController {
+                adressVC.destinyText = sender as? String
+            }
+        }
+        
+        // Getting selected field and updating
+        // our flag in AddressTableViewController
+        if let addressVC = segue.destination as? AddressTableViewController {
+            addressVC.fieldFlag = self.selectedField
+        }
+    }
+    
+    // MARK: - Keyboard toolbar
+    
+    @objc func doneConditionPicker() {
+        self.view.endEditing(true)
     }
 
-    // MARK: - Table view data source
-    
     @IBAction func backToEditRote(_ segue: UIStoryboardSegue) {
         
     }
     
     @IBAction func confirmButton(_ sender: Any) {
-        
         rote?.addRote(rote: Rote(start: start.text ?? "",
                                  destiny: destiny.text ?? "",
-                                 departure: departureTime.text ??  "",
+                                 departure: departureTime.text ?? "",
                                  arrival: arrivalTime.text ?? "" ))
-        
         performSegue(withIdentifier: "backToMatch", sender: self)
-        
     }
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 4
-    }
+    // MARK: - Hour picker view
     
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 2
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    func pickerView(_ pickerView: UIPickerView,
+                    numberOfRowsInComponent component: Int) -> Int {
         
         if component == 0 {
             return hours.count
@@ -121,7 +106,9 @@ class EditRoteTableViewController: UITableViewController, UIPickerViewDelegate, 
         return 0
     }
     
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int,
+                    forComponent component: Int) -> String? {
+        
         if component == 0 {
            return  hours[row]
         } else if component == 1 {
@@ -130,15 +117,17 @@ class EditRoteTableViewController: UITableViewController, UIPickerViewDelegate, 
         return ""
     }
     
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        if departureTime.isFirstResponder{
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int,
+                    inComponent component: Int) {
+        
+        if departureTime.isFirstResponder {
             if component == 0 {
                 selectedHour = hours[row]
             } else if component == 1 {
                 selectedMinute = minutes[row]
             }
             departureTime.text = "\(selectedHour):\(selectedMinute)"
-        } else if arrivalTime.isFirstResponder{
+        } else if arrivalTime.isFirstResponder {
         if component == 0 {
             selectedHour = hours[row]
         } else if component == 1 {
@@ -147,5 +136,52 @@ class EditRoteTableViewController: UITableViewController, UIPickerViewDelegate, 
         arrivalTime.text = "\(selectedHour):\(selectedMinute)"
         }
     }
+    
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        return CGFloat(50.0)
+    }
+    
+    func showDatePicker() {
+        
+        // ToolBar
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Confirmar",
+                                           style: .plain,
+                                           target: self,
+                                           action: #selector(doneConditionPicker))
+        
+        toolbar.tintColor = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
+        toolbar.setItems([spaceButton,cancelButton], animated: false)
+        
+        departureTime.inputAccessoryView = toolbar
+        arrivalTime.inputAccessoryView = toolbar
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if (indexPath.row == 0) {
+            self.selectedField = "start"
+            performSegue(withIdentifier: "selectDestiny", sender: nil)
+        } else if (indexPath.row == 2) {
+            self.selectedField = "destiny"
+            performSegue(withIdentifier: "selectDestiny", sender: nil)
+        }
+        
+    }
 }
