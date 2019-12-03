2a8257f24baed5a030571e5218e2382a8b98df4b
Julia Mestre <juliacmestre@gmail.com>
2019-12-03 10:35:43 -0300

    Alert when finish registration and fix locations screen done

--- CaronaSocial/CaronaSocial/Controller/Registration/AditionalInfoViewController.swift
+++ CaronaSocial/CaronaSocial/Controller/Registration/AditionalInfoViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class AditionalInfoViewController: UIViewController {
     
     @IBOutlet weak var progressImage: UIImageView!
+    @IBOutlet weak var finishButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +25,36 @@ class AditionalInfoViewController: UIViewController {
             progressImage.image = UIImage(named: "progress24")
         } else {
             progressImage.image = UIImage(named: "progress5")
+        }
+    }
+    
+    @IBAction func finishPressed(_ sender: UIButton) {
+        let title = "Cadastro finalizado!"
+        let msg = "Para entrar no aplicativo é preciso verificar seu endereço de email. Por favor confira sua caixa de entrada e siga as instruções no email que enviamos."
+        
+        displayMsg(title : title, msg : msg, style: .alert)
+        
+    }
+    
+    //Alert message
+    func displayMsg(title : String, msg : String, style: UIAlertController.Style = .alert) {
+        
+        let ac = UIAlertController.init(title: title,
+                                        message: msg,
+                                        preferredStyle: style)
+        
+        ac.addAction(UIAlertAction.init(title: "OK",
+                                        style: .default,
+                                        handler: {(action: UIAlertAction!) in
+                                            let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                                            let newRegisterViewController = storyBoard.instantiateViewController(withIdentifier: "Login") as! LoginViewController
+                                            self.navigationController?.pushViewController(newRegisterViewController, animated: true)
+                                            
+        }))
+        
+    
+        DispatchQueue.main.async {
+            self.present(ac, animated: true, completion: nil)
         }
     }
 
