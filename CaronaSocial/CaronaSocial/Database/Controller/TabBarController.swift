e4af3c250dec3d23aa21fde9fc11c7a2f9d4efc7
Matheus Oliveira <matheusdesouzaoliveira@hotmail.com>
2019-12-09 13:55:47 -0300

    Tab bar added

---
+++ CaronaSocial/CaronaSocial/Database/Controller/TabBarController.swift
@@ -0,0 +1,31 @@
+//
+//  TabBarController.swift
+//  CaronaSocial
+//
+//  Created by Matheus Oliveira on 09/12/19.
+//  Copyright © 2019 Matheus Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
